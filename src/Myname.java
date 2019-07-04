f2b229eb2900f36e2cc9567b9834760e21fab48c
user <user@itilite-OptiPlex-5060>
2019-07-04 12:09:21 +0530

    modification in 1st code

--- src/Myname.java
+++ src/Myname.java
@@ -1,7 +1,7 @@
 
 public class Myname {
 	public static void main (String args[]) {
-		System.out.println("My name is Nandhini Muruganandham");
+		System.out.println("My name is lNandhini Muruganandham");
 	}
 
 }
