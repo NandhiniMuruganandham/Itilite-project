c7a80cf8084bfb0163f7829bcea4a89185af7d30
NandhiniMuruganandham <52520051+NandhiniMuruganandham@users.noreply.github.com>
2019-07-04 15:46:43 +0530

    Update Myname.java

--- src/Myname.java
+++ src/Myname.java
@@ -1,13 +1,13 @@
 
 public class Myname {
 	public static void main (String args[]) {
-		System.out.println("My name is Nandhini Muruganandham");
-		System.out.println("Im from vellore");
+		
 		System.out.println("today we are learning git");
 		System.out.println("its intresting");
 		System.out.println("and now im creating new branch");
 		System.out.println("its merging ");
 		System.out.println("another branch");
+		System.out.println("is it so?");
 	}
 
 }
